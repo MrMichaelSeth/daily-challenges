92e387b67d540f5196d18728a69aaf864c96b4a8
Michael Seth <michael.a.seth175@gmail.com>
2021-03-15 13:48:42 -0400

    added code to a function that returns the longest word from any input

--- longest-word/js/script.js
+++ longest-word/js/script.js
@@ -1,4 +1,17 @@
 function LongestWord(sen) {
   // code goes here
-  return sen;
+    sen = sen.replace(/[^a-zA-Z0-9 ]/g, '')
+
+    sen = sen.split(' ');
+
+    let longest = ""; 
+    for (let i = 0; i < sen.length; i++){
+      if (sen[i].length > longest.length){
+        longest = sen[i]
+      }
+
+    }
+  return longest;
 }
+
+console.log(LongestWord("Today I love you more than Yesterday"));
